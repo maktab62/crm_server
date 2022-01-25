0ed08a35ce9cede8d6b98ed62558cb5f3c455b6e
mohammad karimzadeh <mohammad.karimzade10@gmail.com>
2022-01-25 23:30:08 +0330

    category model

--- database/migrate.js
+++ database/migrate.js
@@ -1,9 +1,11 @@
 const Admin = require('./models/admin');
+const Category = require('./models/category');
 
 
 (async function(){
     try{
         await Admin.sync({ alter: true });
+        await Category.sync({ alter: true });
 
     }catch(err){
         console.log("Error connecting to database: ", err);
